e3844e0d50ec2f7174a6b39189ca75f4aec0cdd4
vantoheiuh <bachducnguyenvan9a5@gmail.com>
2023-06-01 14:28:37 +0700

    test

--- linhYu.js
+++ linhYu.js
@@ -2,31 +2,12 @@ const express = require('express')
 
 const app = express()
 const port = 3000
-const urlBingX = "https://api-swap-rest.bingbon.pro/api/v1/user/getBalance";
-const keys = {
-    "akey": "oyyLug3tbuT4MCHxC2wlJFO3yHyEgfleWhDAIXWeDJV3brHMV1VqrhFyXTSez2NxWSiqhYuvt3MS3bsj4AxZA",
-    "skey": "tlMw3C5NDEbALtazN7Qmi1I7ggVWCs5Fp9KlJMe2oCe26VFEMt17SBNojJnelbiDJSzyeVrVTEmBlyfo1Gw"
-};
-let timestamp = Date.now();
-let currency = "USDT";
-let paramString = `apiKey=${keys.akey}&currency=${currency}&timestamp=${timestamp}`
-let originString = `POST/api/v1/user/getBalanceapiKey=${keys.akey}&currency=${currency}&timestamp=${timestamp}`
-console.log(keys.akey, timestamp, currency);
-var profitFx;
+
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.get('/webhook', (req, res) => {
-  res.send('Hello World!')
-})
-app.post('/forex', (req, res) => {
-    console.log(req.body);
-    profitFx = req.body.data.replace('\x00', '');
-    console.log(profitFx)
-    bot.telegram.sendMessage(849997222,`Profit in MT5: ${profitFx}`);
-})
-app.get('/forex', (req, res) => {
-    res.send(profitFx)
+app.get('/', (req, res) => {
+    return res.send('Hello World!');
 })
 app.post('/webhook', (req, res) => {
     console.log(req.body);
